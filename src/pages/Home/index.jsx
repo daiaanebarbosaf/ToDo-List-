78b69fe5d04f7de3d67319d3c38210559d1d08ba
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-01-17 12:28:41 -0300

    add textareae button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
 import '../../styles/global.css';
+import styles from'./Home.module.css';
 
 export function Home() {
 
   return (
     <>
       <Header/>
-      <h1>Corpo</h1>
+    
+      <form className={styles.taskForm}>
+        <textarea 
+          placeholder="Adicione uma nova tarefa"
+        >
+        </textarea>
+
+        <button type="sumit">
+          Criar
+        </button>
+      </form>
     </>
   )
 }
