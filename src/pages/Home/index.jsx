73f52adc1e82d6e63d9ea57b6fdba182c5351d16
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-01-18 12:18:07 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,23 +7,37 @@ import styles from'./Home.module.css';
 export function Home() {
 
   return (
-    <>
+    <div className={styles.container}>
       <Header/>
-      <main>
-        <form className={styles.taskForm}>
-          <textarea 
-            placeholder="Adicione uma nova tarefa"
-          >
-          </textarea>
+      <main className={styles.wrapper}>
+          <form className={styles.taskForm}>
+            <textarea 
+              placeholder="Adicione uma nova tarefa"
+            >
+            </textarea>
 
-          <button 
-            type="sumit"
-          >
-            Criar
-            <TbCirclePlus />
-          </button>
-        </form>
+            <button 
+              type="sumit"
+            >
+              Criar
+              <TbCirclePlus />
+            </button>
+          </form>
+
+          <section className={styles.sectionTasks}>
+            <div className={styles.statusTasks}>
+              <div className={styles.task}>
+                <p className={styles.pTasksCreated}>Tarefas criadas</p>
+                <span>5</span>
+              </div>
+
+              <div className={styles.task}>
+                <p className={styles.pCompletedTasks}>Concluídas</p>
+                <span>2 de 5</span>
+              </div>
+          </div>
+        </section>
       </main>
-    </>
+    </div>
   )
 }
