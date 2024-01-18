91222d5842a065c24d2cb972127b402abeaae10f
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-01-18 12:16:37 -0300

    add icone de circulo

--- src/components/ItemTasks/index.jsx
+++ src/components/ItemTasks/index.jsx
@@ -1,9 +1,11 @@
 import styles from'./ItemTasks.module.css';
 
+import { GoCircle } from "react-icons/go";
+
 export function ItemTasks(){
     return (
-        <div>
-
+        <div className={styles.itemTask}>
+            <GoCircle size={20}/>
         </div>
     )
 }
