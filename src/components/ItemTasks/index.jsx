fcfe54a78ca5e20a594639c564b4fea3434cb4e7
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-01-20 13:14:33 -0300

    reaftorando

--- src/components/ItemTasks/index.jsx
+++ src/components/ItemTasks/index.jsx
@@ -1,11 +1,28 @@
 import styles from'./ItemTasks.module.css';
 
 import { GoCircle } from "react-icons/go";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export function ItemTasks(){
     return (
-        <div className={styles.itemTask}>
-            <GoCircle size={20}/>
+        <div className={styles.task}>
+            <div className={styles.itemsTask}>
+                <header>
+                    <button>
+                        <GoCircle className={styles.goCircle} size={20}/>
+                    </button>
+                    
+                    <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                </header>
+
+                <button title="Deletar tarefa">
+                    <RiDeleteBin6Line size={20}/>
+                </button>
+            </div>
+
+            <span>Time</span>
+            
         </div>
     )
 }
