97e8661f2a71921ff9e77f339f26182a979716d1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-01-24 09:07:24 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,19 @@
 import { Header } from "../../components/Header";
-import { ItemTasks } from "../../components/ItemTasks";
+import { Task } from "../../components/Task";
 
-import { TbCirclePlus } from "react-icons/tb";
-
-import clipBoard from '../../assets/clipboard.svg';
 
 import '../../styles/global.css';
 import styles from'./Home.module.css';
+import { useState } from "react";
 
 const tasks = [
   {
-    id: 1,
-    description: 'Duolingo + Lumosity',
-    publishedAt: new Date('2024-01-22 07:51:00'),
-
-  },
-
-  {
     id: 2,
-    description: 'Leitura',
-    publishedAt: new Date('2024-01-22 07:52:00'),
+    description: 'Vamos que Vamos',
+    publishedAt: new Date('2024-01-22 07:51:00')
   }
-];
+]
+
 
 export function Home() {
 
@@ -29,52 +21,17 @@ export function Home() {
     <div className={styles.container}>
       <Header/>
       <main className={styles.wrapper}>
-          <form className={styles.taskForm}>
-            <textarea 
-              placeholder="Adicione uma nova tarefa"
-            >
-            </textarea>
-
-            <button 
-              type="sumit"
-            >
-              Criar
-              <TbCirclePlus />
-            </button>
-          </form>
-
-          <section className={styles.sectionTasks}>
-            <div className={styles.statusTasks}>
-              <div className={styles.task}>
-                <p className={styles.pTasksCreated}>Tarefas criadas</p>
-                <span>5</span>
-              </div>
-
-              <div className={styles.task}>
-                <p className={styles.pCompletedTasks}>Concluídas</p>
-                <span>2 de 5</span>
-              </div>
-          </div>
-        </section>
-        <div className={styles.line}></div>
-
-        <section className={styles.sectionItemTasks}>
-          {
-              tasks.map(task => {
-                return (
-                  <ItemTasks 
-                    description={task.description}
-                    publishedAt={task.publishedAt}
-                  />
-                )
-              })
-            }
-          <div className={styles.sectionEmpty}>
-            <img src={clipBoard} alt="Icone de uma prancheta" />
-            <p>Você ainda não tem tarefas cadastradas</p>
-            <span>Crie tarefas e organize seus itens a fazer</span>
-          </div>
-        </section>
+        {
+          tasks.map(task => {
+                  return (
+                    <Task
+                      key={task.id}
+                      description={task.description}
+                      publishedAt={task.publishedAt}
+                    />
+                  )
+                })
+              }
       </main>
     </div>
   )
