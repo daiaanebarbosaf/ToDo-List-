03cfc1891865268290167f2797cc7f44915612e8
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-02-20 09:51:01 -0300

    Função para mudar tipo do botão ao completar a tarefa

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,17 +1,36 @@
 import styles from'./Todo.module.css';
 
 import { GoCircle } from "react-icons/go";
-import { FaCheckCircle } from "react-icons/fa";
+import taskCompleted from '../../assets/taskCompleted.svg';
 
 import { RiDeleteBin6Line } from "react-icons/ri";
 
+import React, { useState } from "react";
+
 export function Todo({todo}){
+
+  const [iconColor, setIconColor] = useState('#4EA8DE');
+  const [currentIcon, setCurrentIcon] = useState(<GoCircle />);
+
+  function handleTaskCompleted(){
+    const newIcon = currentIcon.type === GoCircle ? <img src={taskCompleted}/> : <GoCircle />; 
+    setCurrentIcon(newIcon);  
+  }
+
+ 
     return(
         <div className={styles.todo}>
           <header className={styles.itemsTask}>
-            <button>
-                <GoCircle />
-                <FaCheckCircle />
+            <button className={styles.buttonComplete}>
+              {
+                  currentIcon && React.cloneElement(currentIcon, 
+                  {
+                      size: 20,
+                      color: iconColor,
+                      onClick: handleTaskCompleted,
+                  })
+              }
+                
               </button>
               <div className={styles.content}>
                 <p>{todo.id}</p>
@@ -19,7 +38,7 @@ export function Todo({todo}){
               </div>
           </header>
             
-            <button>
+            <button className={styles.buttonDelete}>
               <RiDeleteBin6Line size={20}/>
             </button>
           </div>
