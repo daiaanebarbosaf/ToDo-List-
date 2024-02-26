410ba69f7de1b5408904e091e083e2e9f4e3c5c4
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-02-26 08:53:57 -0300

    Create index.tsx

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,114 @@
+import '../../styles/global.css';
+import styles from'./Home.module.css';
+import { useState } from "react";
+
+import clipBoard from '../../assets/clipboard.svg';
+
+import { Header } from '../../components/Header';
+import { Todo } from '../../components/Todo';
+import { Form } from '../../components/Form';
+
+interface Task {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+  
+}
+
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const checkedTasksCounter = tasks.reduce((prevValue, currentTask) => {
+    if (currentTask.isCompleted) {
+      return prevValue + 1
+    }
+
+    return prevValue
+  }, 0)
+
+  function addTask(text: string){
+    const newTasks = [
+      ...tasks,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        isCompleted: false,
+      },
+    ];
+  
+   setTasks(newTasks);
+  }
+
+  function deleteTask(id: number){
+    
+   const newTasks = [...tasks];
+   const filteredTasks = newTasks.filter((task) =>
+    task.id !== id ? task : null
+   );
+
+   setTasks(filteredTasks);
+  }
+
+  function completeTask(id: number){
+    const newTasks = [...tasks];
+      newTasks.map((task) => 
+        task.id === id ? (task.isCompleted = !task.isCompleted) : task
+      );
+        
+    setTasks(newTasks);
+    
+  }
+
+  return (
+    <div className={styles.container}>
+      <Header />
+
+      <main className={styles.wrapper}>
+        <Form 
+          addTask={addTask}
+        />
+
+        <div className={styles.sectionTasks}>
+            <div className={styles.statusTasks}>
+              <div className={styles.task}>
+                <p className={styles.pTasksCreated}>Tarefas criadas</p>
+                <span>{tasks.length}</span>
+              </div>
+
+              <div className={styles.task}>
+                <p className={styles.pCompletedTasks}>Concluídas</p>
+                <span>{checkedTasksCounter} de {tasks.length}</span>
+              </div>
+          </div>
+        </div>
+
+        <div className={styles.line}></div>
+
+        <div className={styles.listTasks}>
+          {
+            tasks.length === 0 ? 
+                    <div className={styles.sectionEmpty}>
+                    <img src={clipBoard} alt="Icone de uma prancheta" />
+                    <p>Você ainda não tem tarefas cadastradas </p>
+                    <span>Crie tarefas e organize seus itens a fazer</span>
+                  </div>
+            : ""
+          }
+          
+          {
+
+            tasks.map((task) =>(
+              <Todo
+                key={task.id}
+                task={task}
+                deleteTask={deleteTask}
+                completeTask={completeTask}
+              />
+            ))
+          }
+        </div>
+      </main>
+    </div>
+  )
+}
